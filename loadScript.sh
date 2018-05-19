b9ccde73f4b5cf5152e78a6982dbeee35575215c
Aayush Gupta <aayushg2@illinois.edu>
2018-05-18 19:49:08 -0700

    made slight org changes

---
+++ loadScript.sh
@@ -0,0 +1,9 @@
+stow firefox -t stow firefox -t ~/.mozilla/firefox/*.default/chrome/
+stow rofi -t ~/.config/rofi
+stow polybar -t ~/.config/polybar
+stow dunst -t ~/.config/dunst
+stow i3 -t ~/.config/i3
+stow atom -t ~/.atom 
+stow home
+
+
