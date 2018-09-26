c285e6017b1540cb2c7e13d0e7215e28cfb330a0
Aayush Gupta <aayush98@gmail.com>
2018-09-25 19:19:05 -0500

    General update

--- loadScript.sh
+++ loadScript.sh
@@ -2,8 +2,9 @@ stow atom -t ~/.atom
 stow cava -t ~/.config/cava
 stow conky -t ~/.conky
 stow dunst -t ~/.config/dunst
-stow firefox -t stow firefox -t ~/.mozilla/firefox/*.default/chrome/
+stow firefox -t ~/.mozilla/firefox/*.default/chrome/
 stow home
 stow i3 -t ~/.config/i3
 stow polybar -t ~/.config/polybar
 stow rofi -t ~/.config/rofi
+stow neofetch -t ~/.config/neofetch
